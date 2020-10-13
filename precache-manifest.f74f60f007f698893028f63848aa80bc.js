516fd9f952dd3abe899f928a8e06e1c7f95c34d6
Himanshu Jain <jain.himanshu1990@gmail.com>
2020-10-13 12:09:44 +0530

    Updates

---
+++ precache-manifest.f74f60f007f698893028f63848aa80bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a82c4d508855a6686f188b6c8e50ca",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "d318873fb836cba39748",
+    "url": "/Resume/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0ba5ebc900763309dd25",
+    "url": "/Resume/static/css/main.894bbfbc.chunk.css"
+  },
+  {
+    "revision": "d318873fb836cba39748",
+    "url": "/Resume/static/js/2.be4277c5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Resume/static/js/2.be4277c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba5ebc900763309dd25",
+    "url": "/Resume/static/js/main.259853f7.chunk.js"
+  },
+  {
+    "revision": "f1901266f684724560d5",
+    "url": "/Resume/static/js/runtime-main.6aaeef75.js"
+  },
+  {
+    "revision": "cf6261fe97fbfa7079220e13833b2005",
+    "url": "/Resume/static/media/profile_picture.cf6261fe.jpeg"
+  }
+]);
